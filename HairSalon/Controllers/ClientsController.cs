e943d0891d4101835fb6f1d05bfe7dfcccee775b
hmcvay <hayleymcvay8@gmail.com>
2022-03-11 09:31:59 -0800

    create index view passing in list of clients by stylists.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -14,6 +14,12 @@ namespace HairSalon.Controllers
     {
       _db = db;
     }
-    
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
+      return View(model);
+    }
+
   }
 }
