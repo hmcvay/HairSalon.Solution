669bf3262b9309732c621709cf014ac78dd034c6
hmcvay <hayleymcvay8@gmail.com>
2022-03-11 09:39:00 -0800

    Add details view by client id.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -35,5 +35,11 @@ namespace HairSalon.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(Client => Client.ClientId ==id);
+      return View(thisClient);
+    }
+
   }
 }
